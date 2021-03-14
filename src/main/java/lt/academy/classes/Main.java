dbd7d2a8f6197d250aec597f658e92a4a52ae178
GiedriusZar <giedriuz.zareckaz@gmail.com>
2021-03-14 13:49:56 +0200

    2nd mven commit

--- src/main/java/lt/academy/classes/Main.java
+++ src/main/java/lt/academy/classes/Main.java
@@ -4,6 +4,6 @@ package lt.academy.classes;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Maven project");
     }
 }
